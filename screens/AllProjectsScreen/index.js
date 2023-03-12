7af28c926a27a55a75cc8097259dc0f3bde3bdeb
LN <62269693+ln-dev7@users.noreply.github.com>
2023-03-12 21:47:02 +0100

    update

--- screens/AllProjectsScreen/index.js
+++ screens/AllProjectsScreen/index.js
@@ -5,14 +5,45 @@ import {
   ScrollView,
   TouchableOpacity,
 } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import ProjectsList from "../../components/ProjectsList";
+import { COLORS } from "../../tools/contants";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 const AllProjectsScreen = ({
   route: {
     params: { Projects },
   },
+  navigation: { setOptions },
 }) => {
+  useEffect(() => {
+    setOptions({
+      headerTitle: () => (
+        <View
+          style={{
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+            gap: 1,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 17,
+              fontWeight: 700,
+              color: COLORS.secondary,
+            }}
+          >
+            Tout mes projets
+          </Text>
+        </View>
+      ),
+      headerRight: () => (
+        <MaterialCommunityIcons name="fire" color={COLORS.primary} size={30} />
+      ),
+    });
+  }, []);
   return (
     <ScrollView
       style={{
